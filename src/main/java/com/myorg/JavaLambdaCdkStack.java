e08d36a6c60ce76f708a0241cf5a551dc51f5754
Emily <emily.zy.lin@gmail.com>
2025-07-06 22:02:26 -0500

    cdk set-up (test passed)

--- src/main/java/com/myorg/JavaLambdaCdkStack.java
+++ src/main/java/com/myorg/JavaLambdaCdkStack.java
@@ -1,12 +1,21 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
+import java.util.Map;
+
 public class JavaLambdaCdkStack extends Stack {
+
+    //two constructors, with and without props
     public JavaLambdaCdkStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -14,11 +23,12 @@ public class JavaLambdaCdkStack extends Stack {
     public JavaLambdaCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "JavaLambdaCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Function lambdaFunction = new Function(this, "JavaLambdaDemo", FunctionProps.builder()
+                .runtime(Runtime.JAVA_17)
+                .handler("com.example.HelloLambdaDemo::handleRequest")
+                .code(Code.fromAsset("lambda-assets/java-lambda-demo-1.0-SNAPSHOT.jar"))
+                .functionName("java-lambda-cdk")
+                .build()
+        );
     }
 }
