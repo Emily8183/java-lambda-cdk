702ecd60a751e53a4da18e51167bbf4c3466f562
Emily <emily.zy.lin@gmail.com>
2025-08-17 10:54:05 -0500

    update cdkApp with env variables

--- src/main/java/com/myorg/JavaLambdaCdkApp.java
+++ src/main/java/com/myorg/JavaLambdaCdkApp.java
@@ -2,15 +2,40 @@ package com.myorg;
 
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.Environment;
 
 public class JavaLambdaCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
+        String lambdaConnectionArn = getEnvOrThrow("LAMBDA_CONNECTION_ARN");
+        String cdkConnectionArn = getEnvOrThrow("CDK_CONNECTION_ARN");
+
+//      get the account and region from cdk env (After running cdk deploy, default account and region will be auto-generated)
+        String account = getEnvOrThrow("CDK_DEFAULT_ACCOUNT");
+        String region = getEnvOrThrow("CDK_DEFAULT_REGION");
+
+//        create a new environment object
+        Environment deployEnv = Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
+
         new PipelineStack(app, "PipelineStack", StackProps.builder()
-                .build());
+                .env(deployEnv) // 将环境信息传入
+                .build(),
+                lambdaConnectionArn,
+                cdkConnectionArn);
 
         app.synth();
+    }
+
+     private static String getEnvOrThrow(String name) {
+        String value = System.getenv(name);
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("Environment variable '" + name + "' is required but was not found.");
+        }
+        return value;
     }
 }
 
